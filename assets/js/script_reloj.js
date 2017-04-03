26259574433e31dc4a0048d28d10ba9b75ed9ca1
McGiver2017 <xmcgiver12@gmail.com>
2017-04-02 23:56:54 -0500

    40%

---
+++ assets/js/script_reloj.js
@@ -0,0 +1,27 @@
+$("#DateCountdown").TimeCircles();
+$("#PageOpenTimer").TimeCircles();
+
+var updateTime = function(){
+    var date = $("#date").val();
+    var time = $("#time").val();
+    var datetime = date + ' ' + time + ':00';
+    $("#DateCountdown").data('date', datetime).TimeCircles().start();
+}
+$("#date").change(updateTime).keyup(updateTime);
+$("#time").change(updateTime).keyup(updateTime);
+
+// Start and stop are methods applied on the public TimeCircles instance
+$(".startTimer").click(function() {
+    $("#CountDownTimer").TimeCircles().start();
+});
+$(".stopTimer").click(function() {
+    $("#CountDownTimer").TimeCircles().stop();
+});
+
+// Fade in and fade out are examples of how chaining can be done with TimeCircles
+$(".fadeIn").click(function() {
+    $("#PageOpenTimer").fadeIn();
+});
+$(".fadeOut").click(function() {
+    $("#PageOpenTimer").fadeOut();
+});
